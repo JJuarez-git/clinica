c908ac052ad95c900b0bdede1091ccf026eb5b6a
Jose Carlos Juarez <josecarlosjc30@gmail.com>
2021-12-01 19:34:51 +0100

    minor fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { EspecialidadesComponent } from './components/pages/especialidades/especialidades.component';
+import { EquipoComponent } from './components/pages/equipo/equipo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'inicio',  pathMatch: 'full'},
+  /* {path: '**', redirectTo: 404, pathMatch: 'full'}, */
+  {path: 'inicio', component: HomeComponent, pathMatch: 'full'},
+  {path: 'especialidades', component: EspecialidadesComponent, pathMatch: 'full'},
+  {path: 'equipo', component: EquipoComponent, pathMatch: 'full'},
+  {path: 'contacto', component: ContactComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
+//MODULES
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//COMPONENTS
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { EspecialidadesComponent } from './components/pages/especialidades/especialidades.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { EquipoComponent } from './components/pages/equipo/equipo.component';
+
+//MATERIAL
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ContactComponent,
+    EspecialidadesComponent,
+    FooterComponent,
+    EquipoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
