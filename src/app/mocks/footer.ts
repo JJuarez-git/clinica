47ab1e0e9a6d5768d6da67413543f00aba1ce7b6
Jose Carlos Juarez <josecarlosjc30@gmail.com>
2021-12-02 21:50:22 +0100

    footer data

---
+++ src/app/mocks/footer.ts
@@ -0,0 +1,27 @@
+const footerData = {
+    contacto: {
+        title: 'Contacto',
+        info: {
+            movil: '666 333 444',
+            email: 'ineslominchar@gmail.com',
+            localizacion: 'Calle Mayor 38, Lillo Toledo, 45870'
+        }
+    },
+    rrss: {
+        title: 'RRSS',
+        links: {
+            instagram: 'clinicalillo',
+            facebook: 'clinicalillo'
+        }
+    },
+    horario: {
+        title: 'Horario',
+        dias: {
+            lunes_viernes: '00:00 - 00:00',
+            sabado: '00:00 - 00:00',
+            domingo: 'Cerrado'
+        }
+    },
+}
+
+export default footerData
