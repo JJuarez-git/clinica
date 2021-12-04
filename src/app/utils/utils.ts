07ce6ef175ac4354a17d492aba59716aa51acc25
Jose <josecarlosjc30@gmail.com>
2021-12-04 02:07:53 +0100

    utils added

---
+++ src/app/utils/utils.ts
@@ -0,0 +1,8 @@
+import info from '../mocks/info';
+
+export const whatsappUrl = (message:string) =>  {
+   let phoneNum = info.phone.replace(/\s+/g, '')
+   let encodedMsg = encodeURI(message)
+   let uri = `https://wa.me/34${phoneNum}?text=${encodedMsg}`
+   return uri
+}
