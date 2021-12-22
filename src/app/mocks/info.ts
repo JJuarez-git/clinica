deb39d7f7ad72d626356e7cadc64152a578f8001
Jose <josecarlosjc30@gmail.com>
2021-12-23 00:51:43 +0100

    new components added on css

--- src/app/mocks/info.ts
+++ src/app/mocks/info.ts
@@ -1,5 +1,5 @@
 const info = {
-   title: 'Clínica Lillo',
+   title: 'Clínica Dental Lillo',
    phone: '666 333 444',
    email: 'ineslominchar@gmail.com',
    location: 'Calle Mayor 38, Lillo Toledo, 45870'
