1ce5fd5abd0b9d53819b23caf151414658f95082
Jose Carlos Juarez <jjuarez.dev@gmail.com>
2021-12-29 23:24:55 +0100

    New route especialidad by name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { ContactComponent } from './components/pages/contact/contact.component';
 import { EspecialidadesComponent } from './components/pages/especialidades/especialidades.component';
 import { EquipoComponent } from './components/pages/equipo/equipo.component';
+import { EspecialidadComponent } from './components/pages/especialidad/especialidad.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'inicio',  pathMatch: 'full'},
   /* {path: '**', redirectTo: 404, pathMatch: 'full'}, */
   {path: 'inicio', component: HomeComponent, pathMatch: 'full'},
   {path: 'especialidades', component: EspecialidadesComponent, pathMatch: 'full'},
+  {path: 'especialidades/:name', component: EspecialidadComponent},
   {path: 'equipo', component: EquipoComponent, pathMatch: 'full'},
   {path: 'contacto', component: ContactComponent, pathMatch: 'full'}
 ];
