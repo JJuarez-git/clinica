c6f8531f16f175c39e438cf5ca9f33dadf7320ed
Jose Carlos Juarez <josecarlosjc30@gmail.com>
2022-01-07 00:52:51 +0100

    Minor fixes

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import especialidades from '../../../mocks/especialidades';
 import info from 'src/app/mocks/info';
 
 @Component({
