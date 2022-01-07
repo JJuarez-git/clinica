7fc07e80a89e721259c5b45098cf336bbc7303e4
Jose Carlos Juarez <josecarlosjc30@gmail.com>
2022-01-07 23:35:10 +0100

    Home component updated

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import info from 'src/app/mocks/info';
+import { especialidades } from '../../../mocks/especialidades';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,7 @@ export class HomeComponent implements OnInit {
 
   name = info.title
   title = `Inicio | ${info.title}`
+  espData = especialidades
 
   constructor(
     private titleService: Title
@@ -18,6 +20,10 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.titleService.setTitle(this.title)
+  }
+
+  goTop(): void {
+    window.scrollTo(0,0)
   }
 
 }
